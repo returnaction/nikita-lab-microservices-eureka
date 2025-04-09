dabc253c19fdb1bde974a81fb1c67f6bafd56def
returnaction <obergannv@gmail.com>
2025-04-09 06:14:09 +0300

    добавили cors для api gateway  и добавили логер только что то не работает

--- user-service/src/main/java/com/nikita/lab/userservice/controller/UserController.java
+++ user-service/src/main/java/com/nikita/lab/userservice/controller/UserController.java
@@ -2,6 +2,9 @@ package com.nikita.lab.userservice.controller;
 
 import com.nikita.lab.userservice.model.User;
 import com.nikita.lab.userservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,10 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -21,12 +28,14 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
+        logger.info("Instance {} обработал GET /api/users", instanceId);
         List<User> users = userService.findAll();
         return ResponseEntity.ok(users);
     }
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        logger.info("Instance {} Обработал POST /api/users", instanceId);
         User createdUser = userService.createUser(user);
         URI location = URI.create("/api/users/" + createdUser.getId());
 
